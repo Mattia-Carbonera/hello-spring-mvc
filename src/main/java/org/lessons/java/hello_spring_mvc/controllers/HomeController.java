1878d00c11b08dd40c03d81f6edb49fd11245685
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-03-11 16:59:49 +0100

    add home at ("/") & about at ("/about")

---
+++ src/main/java/org/lessons/java/hello_spring_mvc/controllers/HomeController.java
@@ -0,0 +1,25 @@
+package org.lessons.java.hello_spring_mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return new String("index");
+    }
+    
+    
+    @GetMapping("/about")
+    public String about() {
+        return new String("aboutPage");
+    }
+    
+
+}
