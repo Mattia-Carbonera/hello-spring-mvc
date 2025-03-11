0af69b7343c7c43363689c9297f4cf9de1853e3e
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-03-11 17:29:06 +0100

    add contact with param in ("/contacts")

--- src/main/java/org/lessons/java/hello_spring_mvc/controllers/HomeController.java
+++ src/main/java/org/lessons/java/hello_spring_mvc/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package org.lessons.java.hello_spring_mvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +21,13 @@ public class HomeController {
     public String about() {
         return new String("aboutPage");
     }
+
+    @GetMapping("/contacts")
+    public String contacts(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "/contacts";
+    }
+    
     
 
 }
